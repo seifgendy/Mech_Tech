7d49a991d5d5b74d8fad6e5cff68197c971ddc75
seifgendy <141524686+seifgendy@users.noreply.github.com>
2024-09-11 17:57:28 +0300

    Add files via upload

---
+++ Debug/src/main.d
@@ -0,0 +1,26 @@
+src/main.o: ../src/main.c ../src/../include/BIT_MATH.h \
+ ../src/../include/STD_TYPES.h ../src/../include/GPIO_interface.h \
+ ../src/../include/RCC_interface.h ../src/../include/SPI_interface.h \
+ ../src/../include/STK_interface.h ../include/EXTI_interface.h \
+ ../include/NVIC_interface.h ../src/../include/TFT_interfae.h \
+ ../src/../include/Image.h
+
+../src/../include/BIT_MATH.h:
+
+../src/../include/STD_TYPES.h:
+
+../src/../include/GPIO_interface.h:
+
+../src/../include/RCC_interface.h:
+
+../src/../include/SPI_interface.h:
+
+../src/../include/STK_interface.h:
+
+../include/EXTI_interface.h:
+
+../include/NVIC_interface.h:
+
+../src/../include/TFT_interfae.h:
+
+../src/../include/Image.h:
